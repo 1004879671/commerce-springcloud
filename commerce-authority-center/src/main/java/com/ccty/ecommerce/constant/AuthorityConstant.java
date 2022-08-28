9ab41bd5942934856fd886ee890e6e13bc529b2c
caiitianyu <1004879671@qq.com>
2022-08-28 15:11:23 +0800

    初始化

---
+++ commerce-authority-center/src/main/java/com/ccty/ecommerce/constant/AuthorityConstant.java
@@ -0,0 +1,28 @@
+package com.ccty.ecommerce.constant;
+
+/**
+ * <h1>授权需要使用的一些常量信息</h1>
+ * */
+public final class AuthorityConstant {
+
+    /** RSA 私钥, 除了授权中心以外, 不暴露给任何客户端 */
+    public static final String PRIVATE_KEY = "MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCIxOJX/cDn+Cvq+VpklJw0IY" +
+            "3gUPOlVHhNRi088TKyh44iik0OdB6B8rpPnGJ5YGFkfZN7AVMa2IDrkWdZOcDwXjhmSd7p25qCHTA7QM+C6Dnje57oBl+k1UWViZIx4FWj1" +
+            "XPQa3lSyUelD4XwNszQ4RZwIGiaxcVjzlmbtxF3t/UBrm5fH9PnWfr0ShLs0oLOih5u6dh4PIhissg/W4Rfs5zCIFhede6r+hPjKF137Nb1" +
+            "XVXVEPEMSmrsb9285beX3nR00F3i501z/f7RGI+5RWgXac4ODsPpLvsiYfCVXQwe3gpSNYG1lRVZGFbHannlycynNWLz8ekODBmm3e/dAgM" +
+            "BAAECggEAaAYHeZFhBY32KVPacqGtZOEYfcJUbi0fCvcTNUTmoG8FpjO2hAXBpmitWBvDsjmiGcKBR48sXEDLXWBMUMriem/PSc1ppEkI7W" +
+            "mx439Z1WE3llPR+vvOBxrHQANQlGrh+ChDXARCVLsOqPZzks67XSu12zZrGTzO8yhhc1P8DydYiBlItWspPeT2pd1tc0W4pv8/77aS9axdi" +
+            "pVT+kpnQTvdmdJAO7719W1v/1BGemYTRLaIPuVKCvRvwuSCVKMkDypIwhzgRHTv8pIAYXiwobzVRz+l6UZblimvWBiK2tUirWnZEH1C8uxx" +
+            "3sE+0fxiAB9qW4w0adCj00kDybTdqQKBgQDAyPbZG71UhjEbL6JIMZmLuvYPWvtnW+ExvK3n+Y7Z1CQJ9F2D6lQ+MKZyFVO10uzSBsfQmjc" +
+            "beYtpd7hgD+HImhQ1b1kBhKIQ5VEHWDwZwQq/vxtGe6MiY9T3u0rsis3s9VDcGvTv9/PnGHdr/fiU2P17oU2XCnodyKbyzVYVYwKBgQC1nb" +
+            "/4iPSJS+gWjYb0/trwYCyo6UVLm5WyF46oK8JIrcpajtE1vH/Z2plmY5b5iW2b68VJ9IifNEaBZIG5D4Cl8Bs/qprwl2UmANr9L5PsB5aFm" +
+            "xk0HDPpxNgmyiPPMmcdwJU7JWYAhPbPrOLKlDir6AITZgjpmM9E5d1VrKaJvwKBgCCJiogIE4NRhrY06unOXUDlFCRxUZ8XNcHEO4G8b+y5" +
+            "zj9T2/ioswEwCteUW+WDLO8hr9W4QXbEo8/wIdEsvQmzX5fJLDeCYCwD2BIqRTJ/j0XfhkX2Fgl22fnZxYJ4xtqoaLZNYmuh6KtnSLiUS8E" +
+            "rglS8W7ETVpryQ6MVHuNLAoGAIZ1bBMNEj4zvHg1/5ySsnUUPvwu9+aqIYHgfvVysDwuromtAqbOD7myZa+wO64zWLr/Kuk96Yg0o55YUtI" +
+            "37/7qbMIOBPfqnjAsofSgrNsqIv53fYle9/Ayk/Tj4u8bWcxlMBbB8WqhBjlqvd1NL0fWVPMXr7GCOLHBnMQ5NnL0CgYA8coruSZKZ23qWi" +
+            "YW+BWtWfZOlR8Os2Ljb4kxno22LG0wlt+10XLf6iUDdSb/yNq8oSySJKhDuc0qWZZwnR/4ChrYDbI2jc7ZmiX6gX9yWWJC27wsXdbAUvIyF" +
+            "/3jLUHwjeeJVtSWk2rosmo4Tt5aFL3QhkQLSG1/nnU9dnPWmWQ==";
+
+    /** 默认的 Token 超时时间, 一天 */
+    public static final Integer DEFAULT_EXPIRE_DAY = 1;
+}
---
+++ commerce-common/src/main/java/com/ccty/ecommerce/constant/CommonConstant.java
@@ -0,0 +1,19 @@
+package com.ccty.ecommerce.constant;
+
+/**
+ * <h1>通用模块常量定义</h1>
+ * */
+public final class CommonConstant {
+
+    /** RSA 公钥 */
+    public static final String PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiMTiV/3A5/gr6vlaZJScNCGN4FDzpVR" +
+            "4TUYtPPEysoeOIopNDnQegfK6T5xieWBhZH2TewFTGtiA65FnWTnA8F44Zkne6duagh0wO0DPgug543ue6AZfpNVFlYmSMeBVo9Vz0Gt5Us" +
+            "lHpQ+F8DbM0OEWcCBomsXFY85Zm7cRd7f1Aa5uXx/T51n69EoS7NKCzooebunYeDyIYrLIP1uEX7OcwiBYXnXuq/oT4yhdd+zW9V1V1RDxD" +
+            "Epq7G/dvOW3l950dNBd4udNc/3+0RiPuUVoF2nODg7D6S77ImHwlV0MHt4KUjWBtZUVWRhWx2p55cnMpzVi8/HpDgwZpt3v3QIDAQAB";
+
+    /** JWT 中存储用户信息的 key */
+    public static final String JWT_USER_INFO_KEY = "e-commerce-user";
+
+    /** 授权中心的 service-id */
+    public static final String AUTHORITY_CENTER_SERVICE_ID = "commerce-authority-center";
+}
